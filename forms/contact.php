6443c69946dd8cf8f97e612c5e10648bc9c98e25
lacomision <133896769+lacomision@users.noreply.github.com>
2023-05-19 17:33:49 -0300

    Add files via upload

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'elcoccazo@gnail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -34,6 +34,7 @@
   */
 
   $contact->add_message( $_POST['name'], 'From');
+  $contact->add_message( $_POST['subject'], 'Subject')
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
